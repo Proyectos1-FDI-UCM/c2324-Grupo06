580949548ae258b8ad06cc8dc2f08d4c3ae61e42
Reycka <157039413+Reycka@users.noreply.github.com>
2024-02-25 22:57:25 +0100

    Room 3

---
+++ NECROPICKER/Assets/Scripts/CollisionInteracter/Collidables/ReboundOnCollision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReboundOnCollision : MonoBehaviour
+{
+
+
+    private Vector3 NewDirection(Vector3 reboundDirection)
+    {
+
+        return reboundDirection; 
+    }
+}
