4e04b1a0dcc528274eef95c735fcf9de52abac27
Alec <149337496+fwoggydweller@users.noreply.github.com>
2024-02-26 18:35:02 +0100

    Corrección de colisiones, creación de prefab de roca y creación de cadáver de Zaloth

--- NECROPICKER/Assets/Scripts/CollisionInteracter/Collidables/DestroyOnCollision.cs
+++ NECROPICKER/Assets/Scripts/CollisionInteracter/Collidables/DestroyOnCollision.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class DestroyOnCollision : MonoBehaviour, ICollidable
 {
     [SerializeField] float minSpeed = 2;
-    Rigidbody rb;
+    Rigidbody2D rb;
     [SerializeField] LayerMask targetLayer;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
     public void OnCollide(Collider2D other)
     {
-        if (rb.velocity.magnitude > minSpeed && targetLayer == (targetLayer | (1 << other.gameObject.layer))) Destroy(gameObject);
+        if (rb.velocity.magnitude > minSpeed && targetLayer == (targetLayer | (1 << other.gameObject.layer)))
+        {
+            Destroy(gameObject);
+            Debug.Log("AAa");
+        }
+        Debug.Log($"{rb.velocity.magnitude > minSpeed} velocity");
+        Debug.Log($"{targetLayer == (targetLayer | (1 << other.gameObject.layer))} layer");
     }
 }
--- NECROPICKER/Assets/Scripts/CollisionInteracter/Collidables/InstantiateOnCollision.cs
+++ NECROPICKER/Assets/Scripts/CollisionInteracter/Collidables/InstantiateOnCollision.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class InstantiateOnCollision : MonoBehaviour, ICollidable
 {
     [SerializeField] float minSpeed = 2;
-    Rigidbody rb;
+    Rigidbody2D rb;
     [SerializeField] LayerMask targetLayer;
     [SerializeField] private GameObject prefab;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
     public void OnCollide(Collider2D other)
     {
