6e384faae15e9804b1721a83b76658bc31122b6e
fwoggydweller <asanch42@ucm.es>
2024-03-01 13:14:01 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    Animator con código mediante AI

---
+++ NECROPICKER/Assets/Scripts/AISystem/StateMachine/Conditions/PlayerCondition/DirectionCondition.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionCondition : MonoBehaviour, ICondition
+{
+    MovementController movementController;
+    [SerializeField]
+    Vector2 direction;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        movementController = GetComponentInParent<MovementController>();
+    }
+    public bool CheckCondition()
+    {
+        if(movementController.Rb.velocity == Vector2.zero) return false;
+
+        return GreatestVector(movementController.Rb.velocity) == GreatestVector(direction);
+    }
+
+    Vector2 GreatestVector(Vector2 vector)
+    {
+        if(XIsGreater(vector.x, vector.y))
+        {
+            return new Vector2(Mathf.Sign(vector.x), 0);
+        }
+        else
+        {
+            return new Vector2(0, Mathf.Sign(vector.y));
+        }
+    }
+    bool XIsGreater(float x,  float y)
+    {
+        if (Mathf.Abs(x) > Mathf.Abs(y)) return true;
+        else return false;
+    }
+
+    private void OnValidate()
+    {
+        name = "Vector2:(" + direction.x + ", " + direction.y +  ")";
+    }
+}
