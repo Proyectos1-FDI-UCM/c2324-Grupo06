03f0803801586aa3075ea35ddfe82721b5cb023e
Javipopotter <javivals@ucm.es>
2024-03-04 14:32:45 +0100

    a

--- NECROPICKER/Assets/Scripts/AISystem/StateMachine/StateHandler.cs
+++ NECROPICKER/Assets/Scripts/AISystem/StateMachine/StateHandler.cs
@@ -28,8 +28,12 @@ public class StateHandler : MonoBehaviour
     public void ChangeState(State newState)
     {
         currentState.OnStateExit();
+        currentState.gameObject.SetActive(false);
+
         currentState = newState;
+        
         currentState.OnStateEnter();
+        currentState.gameObject.SetActive(true);
     }
 
     void PerformPermanentBehaviours()
