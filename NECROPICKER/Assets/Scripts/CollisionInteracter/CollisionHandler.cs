25bc2fc58f7ea9e4698cc4c272d7e3b8629e282c
Javipopotter <javivals@ucm.es>
2024-03-05 01:03:46 +0100

    Game feel improved

--- NECROPICKER/Assets/Scripts/CollisionInteracter/CollisionHandler.cs
+++ NECROPICKER/Assets/Scripts/CollisionInteracter/CollisionHandler.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] UnityEvent onCollision = new UnityEvent();
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        onCollision?.Invoke();
         ICollidable[] collidables = GetComponents<ICollidable>();
 
         foreach(ICollidable collidable in collidables)
