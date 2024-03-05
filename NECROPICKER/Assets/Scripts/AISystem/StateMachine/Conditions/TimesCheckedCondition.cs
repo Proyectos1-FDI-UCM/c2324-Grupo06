0f9fb59b2bb3f39cacd058a541ed65af011b3d50
Javipopotter <javivals@ucm.es>
2024-03-06 00:06:44 +0100

    TimesCheckedCondition

---
+++ NECROPICKER/Assets/Scripts/AISystem/StateMachine/Conditions/TimesCheckedCondition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimesCheckedCondition : MonoBehaviour, ICondition
+{
+    [SerializeField] int timesChecked = 0;
+    [SerializeField] int timesToCheck = 5;
+
+    public bool CheckCondition()
+    {
+        timesChecked++;
+
+        if(timesChecked >= timesToCheck)
+        {
+            timesChecked = 0;
+            return true;
+        }
+        return false;
+    }
+}
