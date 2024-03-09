226ff01cae34c09905c218f1434dcbb7e3c6a2a1
davidfb1305 <149385933+davidfb1305@users.noreply.github.com>
2024-03-09 09:28:24 +0100

    Prefab de los spikes funcional

---
+++ NECROPICKER/Assets/Scripts/AISystem/StateMachine/Behaviour/ActiveColliderBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class ActiveColliderBehaviour : MonoBehaviour , IBehaviour
+{
+    public bool active = false;
+    private Collider2D _collider;
+    private void Awake()
+    {
+        _collider = GetComponentInParent<Collider2D>();
+    }
+    public void ExecuteBehaviour()
+    {
+        _collider.enabled = active;
+    }
+
+    private void OnValidate()
+    {
+            name = "Active Collider2D"+ active;
+    }
+}
