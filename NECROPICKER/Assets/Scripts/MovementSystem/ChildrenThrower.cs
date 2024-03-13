4cf1e02a1bf4f7e0aa15a6c5a2927570436668e5
Javipopotter <javivals@ucm.es>
2024-03-13 20:33:01 +0100

    added torque to fragments

--- NECROPICKER/Assets/Scripts/MovementSystem/ChildrenThrower.cs
+++ NECROPICKER/Assets/Scripts/MovementSystem/ChildrenThrower.cs
@@ -9,13 +9,19 @@ public class ChildrenThrower : MonoBehaviour
     [SerializeField] float minForce = 10;
     [SerializeField] float maxForce = 20;
 
+    [SerializeField] float minRotforce = -10;
+    [SerializeField] float maxRotforce = 10;
+
     private void Start()
     {
         foreach(Transform child in transform)
         {
             randomDirection = Random.Range(0, 360);
             force = Random.Range(minForce, maxForce);
-            child.GetComponent<Rigidbody2D>().AddForce(new Vector2 (Mathf.Sin(randomDirection) * force, Mathf.Cos(randomDirection) * force), ForceMode2D.Impulse);
+            Rigidbody2D rb = child.GetComponent<Rigidbody2D>();
+
+            rb.AddForce(new Vector2 (Mathf.Sin(randomDirection) * force, Mathf.Cos(randomDirection) * force), ForceMode2D.Impulse);
+            rb.AddTorque(Random.Range(minRotforce, maxRotforce), ForceMode2D.Impulse);
         }
     }
 }
