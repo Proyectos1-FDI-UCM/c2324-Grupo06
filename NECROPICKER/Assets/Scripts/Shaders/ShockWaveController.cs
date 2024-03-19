0aaa68544cced47f55de52264ca169ffdf113e69
Alec <149337496+fwoggydweller@users.noreply.github.com>
2024-03-19 16:17:54 +0100

    Cosa de shader debbugear para ver si va

--- NECROPICKER/Assets/Scripts/Shaders/ShockWaveController.cs
+++ NECROPICKER/Assets/Scripts/Shaders/ShockWaveController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShockWaveController : MonoBehaviour
 {
@@ -13,11 +14,17 @@ public class ShockWaveController : MonoBehaviour
     {
         _material = GetComponent<SpriteRenderer>().material;
     }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CallShockWave();
+        }
+    }
     public void CallShockWave()
     {
-        ShockWaveAction(-0.1f, 0.1f);
+        shockWaveCoroutine = StartCoroutine(ShockWaveAction(-0.1f, 1f));
     }
-
     // Update is called once per frame
     private IEnumerator ShockWaveAction(float startPos, float endPos)
     {
@@ -25,11 +32,14 @@ public class ShockWaveController : MonoBehaviour
 
         float lerpedAmount = 0f;
         float elapsedTime = 0f;
-        while(elapsedTime < shockWaveTime)
+        while (elapsedTime < shockWaveTime)
         {
+            Debug.Log("Si");
             elapsedTime += Time.deltaTime;
-            lerpedAmount = Mathf.Lerp(startPos, endPos, elapsedTime / shockWaveTime);
+
+            lerpedAmount = Mathf.Lerp(startPos, endPos, (elapsedTime / shockWaveTime));
             _material.SetFloat(_waveDistanceFromCenter, lerpedAmount);
+
             yield return null;
         }
     }
