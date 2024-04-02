5e29121b2f67813376631ee76db23b0df4aaf4b5
Alec <149337496+fwoggydweller@users.noreply.github.com>
2024-04-02 19:32:53 +0200

    Sistema de diálogos implementado (falta meter los diálogos finales): se han creado dos prefabs, uno de Daniel (sin el sprite, porque aún no hay) y el del canva con la caja de diálogo.

---
+++ NECROPICKER/Assets/Scripts/Dialogues/Dialogue.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField] private GameObject DialogueMark;
+    private bool isPlayerInRange;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+
+    private int currentDialogue = 0;
+    private bool didDialogueStart;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.X) && !didDialogueStart)
+        {
+            StartDialogue();
+        }
+        else if(dialogueText.text == dialogueLines[currentDialogue] && Input.GetKeyDown(KeyCode.X) && didDialogueStart)
+        {
+            NextDialogue();
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        DialogueMark.SetActive(false);
+        currentDialogue = 0;
+        StartCoroutine(ShowLine());
+    }
+    void NextDialogue()
+    {
+        currentDialogue++;
+        if(currentDialogue < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            DialogueMark.SetActive(true);
+        }
+    }
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach(char ch in dialogueLines[currentDialogue])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<MovementController>() != null)
+        {
+            isPlayerInRange = true;
+            DialogueMark.SetActive(true);
+            Debug.Log("Se puede iniciar diálogo");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<MovementController>() != null)
+        {
+            isPlayerInRange = false;
+            DialogueMark.SetActive(false);
+            Debug.Log("No se puede iniciar diálogo");
+        }
+    }
+}
