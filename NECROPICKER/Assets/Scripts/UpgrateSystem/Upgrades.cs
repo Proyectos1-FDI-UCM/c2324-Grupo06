3a4798c067b2d8cb9cf57de4f4738eff2dddc590
cyntrist <cyntrist@ucm.es>
2024-04-10 10:53:05 +0200

    Upgrades + potions + arm

--- NECROPICKER/Assets/Scripts/UpgrateSystem/Upgrades.cs
+++ NECROPICKER/Assets/Scripts/UpgrateSystem/Upgrades.cs
@@ -7,35 +7,48 @@ using UnityEngine;
 public class Upgrades : ScriptableObject
 {// primera estadistica vida seguida de numero maximo de pociones y velocidad de movimiento
     [SerializeField] private float[] stats = new float[3];
-     GameObject player;
+    [SerializeField] ItemData Pociones;
     [SerializeField] private float Ini_maxhealth = 0;
+    [SerializeField] private int Ini_maxpotionsize = 0;
     [SerializeField] private float Ini_speed = 0;
     
-    public void SetStats()
-    {
-        player = FindAnyObjectByType<InputManager>().gameObject;
+    public GameObject FindPlayer() => FindAnyObjectByType<InputManager>().gameObject;
 
+    void SetStats(GameObject player)
+    {
         player.GetComponent<HealthHandler>().SetMaxHealth(stats[0]);
-        player.GetComponent<HealthHandler>().Heal(10000);
-        //falta potis
+        Pociones.SetMaxStackSize((int)stats[1]);
         player.GetComponent<MovementController>().SetSpeed(stats[2]);
     }
+
     public void Resetstats()
     {
         stats[0] = Ini_maxhealth;
+        stats[1] = Ini_maxpotionsize;
         stats[2] = Ini_speed;
-        SetStats();
+        SetStats(FindPlayer());
 
     }
     public void AddSpeed(float aumento)
     {
         stats[2] += aumento;
-        SetStats();
+        SetStats(FindPlayer());
     }
 
     public void AddMaxHealth(float aumento)
     {
+        GameObject player = FindPlayer();
+
         stats[0] += aumento;
-        SetStats();
+
+        SetStats(player);
+
+        player.GetComponent<HealthHandler>().Heal(10000);
+    }
+    public void AddMaxPotionSize(int aumento)
+    {
+        GameObject player = FindPlayer();
+        stats[1] += aumento;
+        SetStats(player);
     }
 }
