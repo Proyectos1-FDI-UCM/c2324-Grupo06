5a8ca63459d19a9887a0781f2730f5058048c893
fwoggydweller <asanch42@ucm.es>
2024-04-11 20:55:22 +0200

    Escaleras arregladas
    Se muestra un texto que dirá "Click x to go down" cuando te acerques a las escaleras. Actualmente no funciona el load del script On Button Load, no sé si es fallo de mi pc o qué

--- NECROPICKER/Assets/Scripts/OnButtonLoad.cs
+++ NECROPICKER/Assets/Scripts/OnButtonLoad.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OnButtonLoad : MonoBehaviour
 {
+    [SerializeField] private GameObject text;
+    [SerializeField] private ScenesManager scenesManager;
+    [SerializeField] private string scene;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        text.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("A");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        text.SetActive(false);
     }
 }
