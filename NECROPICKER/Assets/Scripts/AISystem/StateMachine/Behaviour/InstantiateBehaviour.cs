84a66aea198ba3e408db030f6fc0c6b6dba05e78
lucasr06 <lucasr06@ucm.es>
2024-04-12 13:09:10 +0200

    Ataque caja boss prefab (WIP)
    Estaba corrupta la escena anterior, he tenido que volver a empezar

--- NECROPICKER/Assets/Scripts/AISystem/StateMachine/Behaviour/InstantiateBehaviour.cs
+++ NECROPICKER/Assets/Scripts/AISystem/StateMachine/Behaviour/InstantiateBehaviour.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class InstantiateBehaviour : MonoBehaviour, IBehaviour
 {
     [SerializeField] GameObject prefab;
     [SerializeField] float velocity = 0.0f;
+    [SerializeField] bool setAnimationOn;
     public void ExecuteBehaviour()
     {
         GameObject bullet = Instantiate(prefab, transform.position, Quaternion.identity);
@@ -14,6 +16,10 @@ public class InstantiateBehaviour : MonoBehaviour, IBehaviour
         {
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             rb.velocity = transform.up * velocity;
+        }
+        if (setAnimationOn)
+        {
+            bullet.AddComponent<Animator>();
         }
     }
 
