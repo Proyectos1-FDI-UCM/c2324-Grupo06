5895c2362c9d50975d83204d55521f08bd6178bb
davidfb1305 <149385933+davidfb1305@users.noreply.github.com>
2024-04-14 09:40:06 +0200

    input implementado en el menuhandler
    Ahora se puede navegar por los menús con los botones tanto de teclado como del mando.

--- NECROPICKER/Assets/Scripts/MenuSystem/FirstBotomSelector.cs
+++ NECROPICKER/Assets/Scripts/MenuSystem/FirstBotomSelector.cs
@@ -5,14 +5,17 @@ using UnityEngine.UIElements;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+
 public class FirstBotomSelector : MonoBehaviour
 {
     [SerializeField] private GameObject bottom;
-    [SerializeField] private EventSystem eventSystem;
+    private EventSystem eventSystem;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        eventSystem.firstSelectedGameObject = bottom;        
+        eventSystem = FindAnyObjectByType<EventSystem>();
+
+        eventSystem.SetSelectedGameObject(bottom);
     }
 
     
