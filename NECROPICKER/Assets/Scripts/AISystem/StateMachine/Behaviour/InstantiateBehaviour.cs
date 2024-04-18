883e23ba32049d09fe7b944a8ce2d634c286bc09
Reycka <157039413+Reycka@users.noreply.github.com>
2024-04-18 11:08:34 +0200

    Fixes para la build
    Se ha nerfeado al Galyundan en velocidad, se hacambiado el scriptable object de las pociones para que aparezcan en 3 aunque a veces falla y se  agregado el scriptable object de las mejoras en el piso del boss
    
    Co-Authored-By: David Fernández <149385933+davidfb1305@users.noreply.github.com>

--- NECROPICKER/Assets/Scripts/AISystem/StateMachine/Behaviour/InstantiateBehaviour.cs
+++ NECROPICKER/Assets/Scripts/AISystem/StateMachine/Behaviour/InstantiateBehaviour.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
-using UnityEditorInternal;
-using UnityEditor.Animations;
+
 using UnityEngine;
 
 public class InstantiateBehaviour : MonoBehaviour, IBehaviour
