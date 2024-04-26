4e47b9f92d441c2b8eed95a8445d2104118938ba
LucasRCucm <149060113+LucasRCucm@users.noreply.github.com>
2024-04-26 12:29:18 +0200

    Cheats added
    Añadidos los cheats para añadir objetos a tu inventario y empezado el sistema de feedback de poca vida

--- NECROPICKER/Assets/Scripts/Input/Cheats.cs
+++ NECROPICKER/Assets/Scripts/Input/Cheats.cs
@@ -8,10 +8,15 @@ public class Cheats : MonoBehaviour
 {
     HealthHandler _myHealthHandler;
 
+    [SerializeField] private Inventory _inventory;
+    [SerializeField] private Inventory _health;
     [SerializeField] private int _healthSetted;
     [SerializeField] private int _maxHealthSetted;
     [SerializeField] private int _healthToHeal;
-    [SerializeField] private GameObject[] _objectsToInstantiate = new GameObject[10];
+    [SerializeField] private ItemData[] _objectsToInstantiate = new ItemData[10];
+    [SerializeField] private int[] _objectsToAdd = new int[10];
+    [SerializeField] private ItemData _potion;
+    [SerializeField] private int _potionsToAdd;
 
     private void Start()
     {
@@ -36,14 +41,61 @@ public class Cheats : MonoBehaviour
         {
             _myHealthHandler.SetMaxHealth(_maxHealthSetted);
         }
-        if (Input.GetKeyDown(KeyCode.C)) //Instanciar objetos
+        if (Input.GetKeyDown(KeyCode.Keypad0)) //Instanciar objetos
         {
-            for(int i = 0; i < _objectsToInstantiate.Length; i++)
+            AddItem(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1)) //Instanciar objetos
+        {
+            AddItem(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2)) //Instanciar objetos
+        {
+            AddItem(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3)) //Instanciar objetos
+        {
+            AddItem(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4)) //Instanciar objetos
+        {
+            AddItem(4);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5)) //Instanciar objetos
+        {
+            AddItem(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6)) //Instanciar objetos
+        {
+            AddItem(6);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad7)) //Instanciar objetos
+        {
+            AddItem(7);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad8)) //Instanciar objetos
+        {
+            AddItem(8);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad9)) //Instanciar objetos
+        {
+            AddItem(9);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            for(int i = 0; i < _potionsToAdd; i++)
             {
-                if (_objectsToInstantiate[i] != null)
-                {
-                    Instantiate(_objectsToInstantiate[i], transform.position, Quaternion.identity);
-                }
+                _health.AddItem(_potion);
+            }
+        }
+    }
+    private void AddItem(int n)
+    {
+        if (_objectsToInstantiate[n] != null)
+        {
+            for (int j = 0; j < _objectsToAdd[n]; j++)
+            {
+                _inventory.AddItem(_objectsToInstantiate[n]);
             }
         }
     }
